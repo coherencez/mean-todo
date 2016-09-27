f506bec98545d5220576371d80d2f81064e5a9fc
Thomas Swann <tkswann@mac.com>
2016-09-26 20:07:43 -0500

    ngRoute connected, first two routes setup

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,18 @@
 'use strict'
 
-const app = angular.module('meanTodo', [])
+const app = angular.module('meanTodo', ['ngRoute'])
+
+app.config($routeProvider => {
+	$routeProvider
+		.when('/', {
+			controller: 'MeanCtrl',
+			templateUrl: 'partials/main.html'
+		})
+		.when('/todos', {
+			controller: 'TodoCtrl',
+			templateUrl: 'partials/todo.html'
+		})
+})
 
 app.controller('MeanCtrl', ['$scope', function($scope) {
 	$scope.title = 'Hello from angular!'
