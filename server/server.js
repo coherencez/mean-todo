03bccd1325021f7f6fa65a4fcf5fd31576ef26e9
Thomas Swann <tkswann@mac.com>
2016-09-26 20:29:03 -0500

    body parser added for parsing json for posting/sending DB results

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@
 
 const express = require('express')
   ,       app = express()
+  ,    {json} = require('body-parser')
   ,      PORT = process.env.PORT || 3000
 
 app.set('port', PORT)
 
 app.use(express.static('client'))
+app.use(json())
 
 app.get('/api/todos', (req,res) => {
 	res.send({
